4c16987c8f7048d31106e675845299b959aad07d
zhenquan.qiu <gnsyxiang@163.com>
2021-11-05 20:02:36 +0800

    feat: add hy_func_check.h

---
+++ include/hy_func_check.h
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_func_check.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    05/11 2021 19:39
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        05/11 2021      create the file
+ * 
+ *     last modified: 05/11 2021 19:39
+ */
+#ifndef __LIBHY_MCU_INCLUDE_HY_FUNC_CHECK_H_
+#define __LIBHY_MCU_INCLUDE_HY_FUNC_CHECK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief 该函数没有实际作用，主要是为了m4宏检测用
+ */
+void HyFuncCheck(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hy_func_check.c
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_func_check.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    05/11 2021 19:46
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        05/11 2021      create the file
+ * 
+ *     last modified: 05/11 2021 19:46
+ */
+#include <stdio.h>
+
+#include "hy_func_check.h"
+
+void HyFuncCheck(void)
+{
+    printf("func check \n");
+}
+
